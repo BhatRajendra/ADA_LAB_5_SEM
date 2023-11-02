92d671e51c30781ac757d4650fdd2150c03575c0
BhatRajendra <120010518+BhatRajendra@users.noreply.github.com>
2023-11-02 10:30:08 +0530

    Create selectionSort.java

---
+++ selectionSort.java
@@ -0,0 +1,52 @@
+import java.time.*;
+import java.util.*;
+public class selectionSort {
+    Random rand=new Random();
+    int[] arr=new int[1000];
+    
+    void sort(){
+        int min,n=arr.length;
+        for(int i=0;i<n;i++){
+            arr[i]=rand.nextInt(100);
+        }
+        //unsorted
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        //now sorting
+        for(int i=0;i<n-2;i++){
+            min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            if(i!=min){
+                int temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp; 
+            }          
+        }
+
+        //sorted print
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        selectionSort obj = new selectionSort();
+
+        long start1 = System.nanoTime();
+        long start2 = System.currentTimeMillis();
+
+        obj.sort();
+        
+        long end1 = System.nanoTime();  
+        long end2 = System.currentTimeMillis(); 
+        
+        
+        System.out.println("Elapsed Time in nano seconds: "+ (end1-start1));      
+        System.out.println("Elapsed Time in milli seconds: "+ (end2-start2));
+    }
+}
